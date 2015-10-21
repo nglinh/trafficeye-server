2318ca75452d89c7680ad55585ccc939ad4bc26b
nglinh <nglinh2312@gmail.com>
2015-10-21 12:15:57 +0800

    added map interface

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { lat: 1.3, lon: 103.8, zoom: 12 });
 });
 
 module.exports = router;
